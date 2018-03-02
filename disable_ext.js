459157384c8ef441398bcf8b3d92ad7b562922bc
Vikrum Nijjar <vikrum@google.com>
2018-03-02 14:35:23 -0800

    Disable Bluetooth.

--- disable_ext.js
+++ disable_ext.js
@@ -1,3 +1,3 @@
 var script = document.createElement('script');
-script.text = '{ navigator.__defineGetter__("usb", () => undefined); }';
+script.text = '{ navigator.__defineGetter__("usb", () => undefined); navigator.__defineGetter__("bluetooth", () => undefined); }';
 document.documentElement.appendChild(script);
